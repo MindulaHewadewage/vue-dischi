5c657670afc0663641b5cf9ce7fa7be90e6ba352
MindulaHewadewage <mindufernando@gmail.com>
2023-03-21 16:52:53 +0100

    fixed form

--- src/pages/SuggestionFormPage.vue
+++ src/pages/SuggestionFormPage.vue
@@ -1,7 +1,7 @@
 <script>
 import axios from 'axios';
 const endpoint = 'http://127.0.0.1:8000/api/videogames';
-const emptyForm = { title: '', publisher: '', description: '', platform: '', genre: '', image: '', price: '', release_Date: '', size: '' };
+const emptyForm = { email: '', subject: '', message: '', subscription: '' };
 
 export default {
     name: 'SuggestionFormPage',
@@ -14,6 +14,8 @@ export default {
         sendForm() {
             axios.post(endpoint, this.form)
                 .then(() => { this.form = emptyForm })
+                .catch(err => { console.error(err) })
+                .then(() => { })
         }
     }
 }
@@ -28,92 +30,36 @@ export default {
         <form novalidate @submit.prevent="sendForm">
             <div class="container">
                 <div class="row">
-                    <!-- TITOLO -->
-                    <div class="col-6">
-                        <div class="mb-3">
-                            <label for="title" class="form-label">Titolo:</label>
-                            <input type="text" class="form-control" id="title" placeholder="" name="title">
-                        </div>
-                    </div>
-
-                    <!-- Publisher -->
-                    <div class="col-6">
-                        <div class="mb-3">
-                            <label for="publisher" class="form-label">Publisher:</label>
-                            <input type="text" class="form-control" id="publisher" placeholder="" name="publisher">
-                        </div>
-                    </div>
-
-
-                    <!-- Descrizione -->
+                    <!-- EMAIL -->
                     <div class="col-12">
                         <div class="mb-3">
-                            <label for="description" class="form-label">Descrizione</label>
-                            <textarea class="form-control" id="description" rows="3" name="description"></textarea>
+                            <label for="email" class="form-label">Email</label>
+                            <input type="email" class="form-control" id="email" placeholder="nome@mail.com" name="email"
+                                v-model.trim="form.email" required>
                         </div>
                     </div>
 
-
-                    <!-- Piattaforma -->
-                    <div class="col-3">
+                    <!-- OGGETTO -->
+                    <div class="col-12">
                         <div class="mb-3">
-                            <label for="platform" class="form-label">Piattaforma:</label>
-                            <select class="form-select" name="platform" id="Piattaforma">
-                                <option value="">PC</option>
-                                <option value="">PS4 </option>
-                                <option value="">XBOX</option>
-                                <option value="">SWITCH</option>
-                            </select>
+                            <label for="subject" class="form-label">Oggetto</label>
+                            <input type="text" class="form-control" id="subject" placeholder="" name="subject"
+                                v-model.trim="form.subject" required>
                         </div>
                     </div>
 
-                    <!-- Genere -->
-                    <div class="col-3">
+
+                    <!-- CONTENUTO MESSAGGIO -->
+                    <div class="col-12">
                         <div class="mb-3">
-                            <label for="genre" class="form-label">Genere:</label>
-                            <select class="form-select" name="genre" id="genre">
-                                <option value="">Horror</option>
-                                <option value="">Fantasy</option>
-                                <option value="">RPG</option>
-                                <option value="">Sparatutto</option>
-                            </select>
-                        </div>
-                    </div>
-
-                    <!-- Immagine Copertina -->
-                    <div class="col-6">
-                        <div class="mb-3">
-                            <label for="image" class="form-label">Immagine:</label>
-                            <input type="url" class="form-control" id="image" placeholder="Immagine di copertina"
-                                name="image">
-                        </div>
-                    </div>
-
-                    <!-- Prezzo -->
-                    <div class="col-4">
-                        <div class="mb-3">
-                            <label for="price" class="form-label">Prezzo:</label>
-                            <input type="number" class="form-control" id="price" placeholder="Es.€34.99" name="price">
-                        </div>
-                    </div>
-
-                    <!-- Data di rilascio -->
-                    <div class="col-4">
-                        <div class="mb-3">
-                            <label for="release_date" class="form-label">Data di rilascio:</label>
-                            <input type="date" class="form-control" id="release_date" placeholder="Data di rilascio"
-                                name="release_date">
+                            <label for="message" class="form-label">Contenuto del messaggio</label>
+                            <textarea class="form-control" id="message" rows="3" name="message" v-model.trim="form.message"
+                                required></textarea>
                         </div>
                     </div>
 
 
-                    <!-- Peso -->
-                    <div class="col-4">
-                        <div class="mb-3">
-                            <label for="size" class="form-label">Peso:</label>
-                            <input type="number" class="form-control" id="size" placeholder="Es.62 GB" name="size">
-                        </div>
-                    </div>
+
 
                     <!-- Iscrizione Newsletter -->
                     <div class="form-check form-check-inline">
